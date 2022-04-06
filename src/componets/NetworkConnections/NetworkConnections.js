8aae9c7e4b092436d133d88bbcb6ab02eac553ab
JOJOKE21 <kingjordan2122@gmail.com>
2022-04-05 21:33:46 -0400

    testing again

--- src/componets/NetworkConnections/NetworkConnections.js
+++ src/componets/NetworkConnections/NetworkConnections.js
@@ -1,4 +1,4 @@
-const apikey = bed48dd19daf5f0b7bfc95478a74372b
+const apikey = process.env.REACT_APP_API_KEY
 
 export const fetchMoviesData = (route, query = '') => {
     return fetch(`https://api.themoviedb.org/3${route}?api_key=${apikey}${query}`)
